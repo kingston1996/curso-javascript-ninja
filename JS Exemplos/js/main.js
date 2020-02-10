fbeda406d706c19ca23c771b4303e32d06def6be
Jonatas Ferreira <silvajonatas2@gmail.com>
2020-02-10 20:53:52 -0300

    Adicionando a pasta JS Exemplos

---
+++ JS Exemplos/js/main.js
@@ -0,0 +1,4 @@
+(function(){
+	var name = 'Variavel Local na IIFE';
+	console.log(name);
+})();
---
+++ JS Exemplos/js/main2.js
@@ -0,0 +1,4 @@
+(function(){
+	var name = 'Variavel Local na IIFE no main2';
+	console.log(name);
+})();
