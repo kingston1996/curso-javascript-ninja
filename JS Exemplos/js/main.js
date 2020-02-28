a3e686c4dce89eaedd95567ae65175d7a38a4e47
Jonatas <silvajonatas2@gmail.com>
2020-02-28 18:15:23 -0300

    Js Exemplos aula17

--- JS Exemplos/js/main.js
+++ JS Exemplos/js/main.js
@@ -1,32 +1,41 @@
 (function(){
-	function myFunction(){
-		return this;
-	}
-	console.log('myFunction', myFunction(), myFunction() === window);
+	var texto = 'O tempo é muito lento 30 minutos para os que esperam Muito rápido 1 hora para os que têm medo Muito longo para os que lamentam Muito curto para os que festejam para os que amam, o tempo é eterno.';
+	console.log(texto);
+	/*console.log(texto.match(/p/));
+	console.log(texto.match(/m/g));
+	console.log(texto.match(/\w/));
+	console.log(texto.match(/\w/g));
+	console.log(texto.match(/esperam/));
+	console.log(texto.match(/dalto/));
+	console.log(texto.match(/\d/));
+	console.log(texto.match(/\d/g));
+	console.log(texto.match(/\d\d/));
+	console.log(texto.match(/\d\d/g));
+	console.log(texto.match(/(ui)/g));
+	console.log(texto.match(/[ab]/g));
+	console.log(texto.match(/[(ab)(cd)]/g));
+	console.log(texto.match(/[a-z]/g));
+	console.log(texto.match(/[A-Za-z0-9]/g));
+	console.log(texto.replace('O','o'));
+	console.log(texto.replace(/O/,'Do'));
+	console.log(texto.replace(/o/g,'Do'));*/
+	console.log(texto.replace(/(para)/g, '--$1--'));
+	console.log(texto.replace(/(l)(e)/g, '--$2--'));
+	console.log(texto.replace(/(l)(e)/g,  function(){
+		console.log(arguments);
+	}));
+	console.log(texto.replace(/(l)(e)/g,  function(capturaTotal, l, e){
+		return (l + e).toUpperCase();
+	}));
+	var nome = 'Jonatas';
+	console.log(nome);
+	console.log(nome.replace(/(\w)/g, function(capturaTotal, letra){
+		return letra.toUpperCase();
+	}));
+	console.log(nome.replace(/(\w)(\w)/g, function(capturaTotal, letra1, letra2){
+		return letra1.toUpperCase() + letra2.toLowerCase();
+	}));
 
-	var myObject = {
-		myProperthy : 1,
-		init : function init(){
-			return this;
-		}
-	};
-	console.log('myObject',myObject.init(), myObject.init() === myObject);
-
-	var newObject = new Object();
-	console.log(newObject);
-
-	// criando meu próprio construtor
-	function myContructor(){
-		this.prop1 = 'prop1';
-		this.prop2 = 'prop2';
-	}
-	var construtor = new myContructor();
-	construtor.prop1 = 'Propriedade 1';
-	console.log('Contructor', construtor.prop1, construtor.prop2);
 
 
-	function myFunction(arg1, arg2){
-		return arguments;
-	}	
-	console.log('myFunction' ,myFunction(1,2));
 })();
