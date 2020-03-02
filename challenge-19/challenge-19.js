4ba0484a757250b18f9d4d7b83fa4ed3e6c3705a
Jonatas <silvajonatas2@gmail.com>
2020-03-02 19:00:39 -0300

    challenge-19 completo

--- challenge-19/challenge-19.js
+++ challenge-19/challenge-19.js
@@ -87,5 +87,16 @@ para exemplificar.
 */
 var markup = '<main>\n  <div class="container">\n    <span class="text date"></span>\n    <p class=\'excerpt\'></p>\n  </div>\n</main>';
 console.log( '\nQuais classes CSS existem na marcação abaixo?\n\n', markup, '\n' );
+
+function hasClass(markup, cssClass){
+	var regex = new RegExp('class=["\'](?:[\\w\\s]+)?'+ cssClass +'(?:[\\w\\s]+)?["\']');
+	return regex.test(markup);
+}
+var classes = ['container', 'text', 'date', 'excerpt', 'main'];
+classes.forEach(function(cssClass){
+	console.log(hasClass(markup, cssClass) +  ' para a classe ' + cssClass);
+});
+
+
 // ?
 })();
