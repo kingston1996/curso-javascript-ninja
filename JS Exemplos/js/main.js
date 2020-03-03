a9c2d407cded4be736b4e7e48ce8b489f697a08b
Jonatas <silvajonatas2@gmail.com>
2020-03-03 20:32:29 -0300

    Js exemplos Aula 21

--- JS Exemplos/js/main.js
+++ JS Exemplos/js/main.js
@@ -1,55 +1,9 @@
 (function(win, doc){
 	'use strict';
-	/*if(win === window)
-	console.log('win é um referência global');
-	console.log('Essa mensagem sempre será exibida! ');
-
-	
-	var obj = {
-		prop1: {
-			prop2: { prop3: 1}
-		}
-	}
-	with(obj.prop1.prop2){
-		console.log(prop3);
-	}
-
-	alert('Exibindo mensagem no browser!');
-	
-	if(prompt('Você é top?'))
-		console.log('Para de ser!');
-	else
-		console.log('Ainda bem até outro dia...');
-	
-	var pergunta = prompt('Salve, está bem?');
-	if(pergunta)
-		console.log('legal...');
-	else
-		console.log('Fala logo desgraça...');
-	
-	var perg = confirm('Deseja mesmo ser top?');
-	if(perg)
-		console.log('Se tornou top mesmo!!', perg)
-	else
-		console.log('Continua pé di rato então....', perg);
-	console.log(doc.getElementById('my-link'));
-	console.log(doc.getElementsByClassName('my-link'));
-	
-	
-	var $inputs = doc.getElementsByTagName('input');
-	console.log($inputs.length);
-*/
-	var $inputUsername = doc.querySelector('#username');
-	var $inputPassword = doc.querySelector('#password');
-	var $button 	   = doc.querySelector('#button');
-	$inputUsername.value = 'Dalto Dunga';
-	$inputPassword.value = 'senha';
-	$button.addEventListener('click', function(event){
-		event.preventDefault();
-		console.log('Click no botão!');
+	console.log('ínicio');
+	document.addEventListener('click', function(){
+		console.log('clickou no documento!');
 	}, false);
-	$inputUsername.addEventListener('click', function(event){
-		alert('Clickou no input');
-	}, false);
-	console.log($inputUsername.value, $inputPassword.value);
+	console.log('fim');
+
 })(window, document);
