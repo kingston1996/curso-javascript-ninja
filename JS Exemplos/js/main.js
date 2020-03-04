7c1ce2c326e597ab0e3d374043673fedd0b110ea
Jonatas <silvajonatas2@gmail.com>
2020-03-04 11:23:15 -0300

    Js exemplos aula21

--- JS Exemplos/js/main.js
+++ JS Exemplos/js/main.js
@@ -1,9 +1,51 @@
 (function(win, doc){
 	'use strict';
-	console.log('ínicio');
-	document.addEventListener('click', function(){
-		console.log('clickou no documento!');
-	}, false);
+	/*console.log('ínicio');
+	setTimeout(function(){
+		console.log('Executou o setTimeout em 1000ms');
+	}, 1000);
+	setInterval(function(){
+		console.log('Executou o setInterval em 1000ms');
+	}, 1000);
+	var counter = 0;
+	function timer(){
+		console.log('Timer', counter++);
+		if(counter > 10)
+			return console.log('Fim da Função Timer!');
+			setTimeout(timer, 1000);
+	}
+	timer();
 	console.log('fim');
+	var counter = 0;
+	function timer(){
+		console.log('Timer: ', counter++);
+		if(counter > 10)
+			return;
+		setTimeout(timer, 1000);
+	}
+	timer();
 
+
+	function timer(){
+		console.log('Timer: ', counter++);
+
+		setInterval(timer, 1000);
+	}
+	timer();*/
+
+	var counter = 0;
+	var $button = doc.querySelector('[data-js="button"]');
+	var temporizador;
+	function timer(){
+		console.log('Timer: ', counter++);
+		if(counter > 20)
+			return;
+		temporizador = setTimeout(timer, 1000);
+		//console.log('temporizador: ', temporizador);
+	}
+	timer();
+
+	$button.addEventListener('click', function(){
+		clearTimeout(temporizador);
+	}, false);
 })(window, document);
