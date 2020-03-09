9ab2187ba6365e45883d1b7885685ffc60149978
Jonatas <silvajonatas2@gmail.com>
2020-03-09 16:14:26 -0300

    30% concluido

--- challenge-23/challenge-23.js
+++ challenge-23/challenge-23.js
@@ -30,6 +30,13 @@ var $visor = doc.querySelector('[data-js="visor"]');
 var $buttonsNumbers = doc.querySelectorAll('[data-js="button-number"]');
 
 Array.prototype.forEach.call($buttonsNumbers, function (button){
-  console.log(button);
+  button.addEventListener('click', handleClickNumber, false);
 });
+
+  function handleClickNumber(event){
+    $visor.value = $visor.value + this.value;
+  }
+
+
+
 })(window, document);
