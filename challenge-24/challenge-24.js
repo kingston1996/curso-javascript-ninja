799293718ec8a3d31ed0ba44e18d8ee6b581fc49
Jonatas <silvajonatas2@gmail.com>
2020-03-18 10:42:31 -0300

    100% concluido

--- challenge-24/challenge-24.js
+++ challenge-24/challenge-24.js
@@ -11,7 +11,6 @@ listeners de eventos, etc);
 - faça refactories para melhorar esse código, mas de forma que o mantenha com a
 mesma funcionalidade.
 */
-
 var $visor = document.querySelector('[data-js="visor"]');
 var $buttonsNumbers = document.querySelectorAll('[data-js="button-number"]');
 var $buttonsOperations = document.querySelectorAll('[data-js="button-operation"]');
@@ -23,16 +22,15 @@ function initialize(){
 }
 
 function initEvents(){
-Array.prototype.forEach.call($buttonsNumbers, function(button) {
+  Array.prototype.forEach.call($buttonsNumbers, function(button) {
     button.addEventListener('click', handleClickNumber, false);
-});
-Array.prototype.forEach.call($buttonsOperations, function(button) {
-  button.addEventListener('click', handleClickOperation, false);
-});
-$buttonCE.addEventListener('click', handleClickCE, false);
-$buttonEqual.addEventListener('click', handleClickEqual, false);
+  });
+  Array.prototype.forEach.call($buttonsOperations, function(button) {
+    button.addEventListener('click', handleClickOperation, false);
+  });
+  $buttonCE.addEventListener('click', handleClickCE, false);
+  $buttonEqual.addEventListener('click', handleClickEqual, false);
 }
-
 
 function handleClickNumber() {
   $visor.value += this.value;
@@ -48,46 +46,60 @@ function handleClickCE() {
 }
 
 function isLastItemAnOperation(number) {
-  var operations = ['+', '-', 'x', '÷'];
+  var operations = getOperations();
   var lastItem = number.split('').pop();
   return operations.some(function(operator) {
     return operator === lastItem;
   });
 }
 
+function getOperations(){
+  //debugger;
+  return Array.prototype.map.call($buttonsOperations, function(button){
+    return button.value;
+  });
+}
+
 function removeLastItemIfItIsAnOperator(string) {
-  if(isLastItemAnOperation(string)) {
+  if(isLastItemAnOperation(string))
     return string.slice(0, -1);
-  }
   return string;
 }
 
 function handleClickEqual() {
   $visor.value = removeLastItemIfItIsAnOperator($visor.value);
-  var allValues = $visor.value.match(/\d+[+x÷-]?/g);
+  var allValues = $visor.value.match(getRegexOperations());
   $visor.value = allValues.reduce(calculateAllValues);
 }
 
-
-function calculateAllValues(accumulated, actual) {
-    var firstValue = accumulated.slice(0, -1);
-    var operator = accumulated.split('').pop();
-    var lastValue = removeLastItemIfItIsAnOperator(actual);
-    var lastOperator = isLastItemAnOperation(actual) ? actual.split('').pop() : '';
-    return doOperation(operator, firstValue, lastValue) + lastOperator;
-  }
-function doOperation(operator, firstValue, lastValue){
-  switch(operator) {
-      case '+':
-        return  Number(firstValue) + Number(lastValue) ;
-      case '-':
-        return  Number(firstValue) - Number(lastValue) ;
-      case 'x':
-        return  Number(firstValue) * Number(lastValue) ;
-      case '÷':
-        return  Number(firstValue) / Number(lastValue) ;
-    }
+function getRegexOperations(){
+  return new RegExp('\\d+[' + getOperations().join('') + ']?', 'g');
 }
 
-  initialize();
+function calculateAllValues(accumulated, actual) {
+  var firstValue = accumulated.slice(0, -1);
+  var operator = accumulated.split('').pop();
+  var lastValue = removeLastItemIfItIsAnOperator(actual);
+  var lastOperator = getLastOperator(actual);
+  return doOperation(operator, firstValue, lastValue) + lastOperator;
+}
+
+function getLastOperator(value){
+  return isLastItemAnOperation(value) ? value.split('').pop() : '';
+}
+
+function doOperation(operator, firstValue, lastValue){
+  switch(operator) {
+    case '+':
+    return  Number(firstValue) + Number(lastValue) ;
+    case '-':
+    return  Number(firstValue) - Number(lastValue) ;
+    case 'x':
+    return  Number(firstValue) * Number(lastValue) ;
+    case '÷':
+    return  Number(firstValue) / Number(lastValue) ;
+  }
+}
+
+initialize();
 })(window, document);
