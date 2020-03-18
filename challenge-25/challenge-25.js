96364cd3ea1de8b944ed8c5f9d60574da4e2cf6a
Jonatas <silvajonatas2@gmail.com>
2020-03-18 19:57:38 -0300

    100% concluido

--- challenge-25/challenge-25.js
+++ challenge-25/challenge-25.js
@@ -1,4 +1,5 @@
 (function(win, doc){
+  'use strict';
 /*
 Essa semana você terá dois desafios:
 1) Revisar todo o contéudo passado até aqui, e ver se você realmente entendeu
@@ -15,5 +16,52 @@ https://developer.mozilla.org/en-US/docs/Web/Events#Categories
 Tente aplicar na prática alguns dos eventos que estão ali e coloque nesse
 desafio os experimentos legais que você conseguir desenvolver :D
 */
+
+//Revisando Conteúdo
+
+function on(element, event, callback){
+  document.querySelector(element).addEventListener(event, callback, false);
+}
+
+function off(element, event, callback){
+  document.querySelector(element).removeEventListener(event, callback, false);
+}
+
+function handleClick(event){
+  event.preventDefault();
+  alert('Clicou no LINKKKKK!!!! :P');
+}
+
+on('[data-js="link"]', 'click', handleClick);
+
+//passando um elemento a mais alternativo
+
+on('[data-js="link"]', 'click', function(event){
+  event.preventDefault();
+  alert('Clicou no link e recebeu essa mensagem ALTERNATIVA!!');
+});
+
+//ignorando o 1º on passado na função
+off('[data-js="link"]', 'click', handleClick);
+/*
+//input
+on('[data-js="input"]', 'input', function(event){
+  console.log(this.value);
+});
+
+//keyup
+on('[data-js="input"]', 'keyup', function(event){
+  console.log(this.value);
+});*/
+
+//keydown
+on('[data-js="input"]', 'keydown', function(event){
+  console.log(this.value);
+});
+
+//change
+on('[data-js="select"]', 'change', function(event){
+  document.querySelector('[data-js="input"]').value = this.value;
+});
 
 })(window, document);
